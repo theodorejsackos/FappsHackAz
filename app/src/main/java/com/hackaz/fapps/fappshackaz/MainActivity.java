4b2341ba93ef78ca27ae4e5dcd30ea3e85082615
Benjamin Shields <benjaminshields@email.arizona.edu>
2017-01-14 05:34:29 -0700

    Printing lists of installed apps in debug message

--- app/src/main/java/com/hackaz/fapps/fappshackaz/MainActivity.java
+++ app/src/main/java/com/hackaz/fapps/fappshackaz/MainActivity.java
@@ -1,12 +1,17 @@
 package com.hackaz.fapps.fappshackaz;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import static android.content.pm.PackageManager.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +22,47 @@ public class MainActivity extends AppCompatActivity {
         Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
         mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
         List<ResolveInfo> pkgAppList = this.getPackageManager().queryIntentActivities(mainIntent, 0);
+        Log.d("Fappshackaz", "First size: "+pkgAppList.size());
         for(int i = 0; i < pkgAppList.size(); i++)
             Log.d("Fappshackaz", pkgAppList.get(i).toString());
 
+        // TRY GETTING USER-INSTALLED APPS
+        int flags = GET_META_DATA |
+                GET_SHARED_LIBRARY_FILES;// |
+                //GET_UNINSTALLED_PACKAGES;
+        PackageManager pm = getPackageManager();
+        List<ApplicationInfo> applications = pm.getInstalledApplications(flags);
+        List<ApplicationInfo> systemInstalled = applications;
+        List<ApplicationInfo> userInstalled = new LinkedList<>();
+        int numTotal = applications.size();
+        int numSys = systemInstalled.size();
+        int numUser = userInstalled.size();
+        Log.d("NUMBER OF APPS", "***\nTotal: "+numTotal+"\nUser: "+numUser+"\nSystem: "+numSys+"***\n");
+
+        ApplicationInfo appInfo;
+        for (int i = 0; i < applications.size(); i++) {
+            appInfo = applications.get(i);
+            if ((appInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1) {
+                // System application
+            }
+            else {
+                // Installed by user
+                userInstalled.add(appInfo);
+                systemInstalled.remove(appInfo); // removeIf?
+            }
+        }
+        numTotal = applications.size();
+        numSys = systemInstalled.size();
+        numUser = userInstalled.size();
+        if (numSys + numUser == numTotal) {
+            Log.d("NUMBER OF APPS", "*** Looking A O K boss! ***");
+        }
+        else {
+            Log.d("NUMBER OF APPS", "***\nTotal: "+numTotal+"\nUser: "+numUser+"\nSystem: "+numSys+"***\n");
+        }
+        Log.d("****-----****\nLONG LIST", "lsadfjlaksfda;slfj");
+        for(int i = 0; i < applications.size(); i++)
+            Log.d("SOMETHING ELSE", applications.get(i).toString());
+        // TRY GETTING USER-INSTALLED APPS
     }
 }
