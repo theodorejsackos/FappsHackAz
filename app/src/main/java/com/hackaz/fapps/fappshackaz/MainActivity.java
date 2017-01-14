70c7955b9aaca744eb98032bff656e8a1a4dab52
Theodore Sackos <theodorejsackos@email.arizona.edu>
2017-01-14 01:54:29 -0700

    Log.d printing of all installed applications

--- app/src/main/java/com/hackaz/fapps/fappshackaz/MainActivity.java
+++ app/src/main/java/com/hackaz/fapps/fappshackaz/MainActivity.java
@@ -1,7 +1,12 @@
 package com.hackaz.fapps.fappshackaz;
 
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> pkgAppList = this.getPackageManager().queryIntentActivities(mainIntent, 0);
+        for(int i = 0; i < pkgAppList.size(); i++)
+            Log.d("Fappshackaz", pkgAppList.get(i).toString());
+
     }
 }
