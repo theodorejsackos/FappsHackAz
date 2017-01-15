6969131f99cedfa8309bdbaaec5eb8396e657014
Theodore Sackos <theodorejsackos@email.arizona.edu>
2017-01-15 00:57:25 -0700

    Starting on database interface

--- app/src/main/java/com/hackaz/fapps/fappshackaz/MainActivity.java
+++ app/src/main/java/com/hackaz/fapps/fappshackaz/MainActivity.java
@@ -18,6 +18,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -330,5 +331,11 @@ public class MainActivity extends AppCompatActivity {
         ApplicationInfo ai = null;
         String result = "";
         return (String) (ai != null ? pm.getApplicationLabel(ai) : "(unknown)");
+    }
+
+
+    private class ProfileNode implements Serializable{
+        public UserDemographic ud;
+        public TradeCraft tc;
     }
 }
